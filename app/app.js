47c86171f96f28de7220e6ae8d0bb7621e176678
Simon Denier <simon@sogilis.com>
2015-03-21 16:50:01 +0100

    display polling status

--- app/app.js
+++ app/app.js
@@ -5,22 +5,33 @@ var App = {
     angular.module('golive', []).controller('MainCtrl', function($scope, $http, $interval) {
 
       function pollData() {
+        var previousStatus = $scope.pollingStatus;
+        $scope.pollingStatus = 'Polling';
         $http.get('/lastresults.json').success(function(data) {
           $scope.lastTime = data.lastTime;
           $scope.stageName = data.name;
           $scope.courses = data.courses;
+          $scope.pollingStatus = previousStatus;
         });
       }
+
+      $scope.pollingStatus = 'Stopped';
+
+      $scope.refresh = function() {
+        pollData();
+      };
+      pollData();
 
       var polling;
       $scope.start = function() {
         polling = $interval(pollData, 5000);
-      };
-      $scope.stop = function() {
-        $interval.cancel(polling);
+        $scope.pollingStatus = 'Running';
       };
 
-      pollData();
+      $scope.stop = function() {
+        $interval.cancel(polling);
+        $scope.pollingStatus = 'Stopped';
+      };
 
     });
   }
