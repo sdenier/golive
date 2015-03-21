6377ebd23ea49b7c0f1dd648a175302b0e84c3d7
Simon Denier <simon@sogilis.com>
2015-03-21 19:45:44 +0100

    delay autoscroll depending on column position
    do not scroll all columns at the same time (less distracting)

--- app/app.js
+++ app/app.js
@@ -44,22 +44,26 @@ var App = {
       };
 
     })
-    .directive('glAutoScroll', function($interval, glConstants) {
+    .directive('glAutoScroll', function($timeout, $interval, glConstants) {
       return {
         restrict: 'A',
-        scope: {},
+        scope: {
+          delay: '=glAutoScroll'
+        },
         link: function(scope, element) {
-          var scrolling = $interval(function() {
-            var scrollTop = element[0].scrollTop,
-                scrollHeight = element[0].scrollHeight,
-                clientHeight = element[0].clientHeight,
-                atEnd = scrollTop + clientHeight >= scrollHeight,
-                newScrollTop = atEnd ? 0 : scrollTop + glConstants.scroll.step;
+          $timeout(function() {
+            var scrolling = $interval(function() {
+              var scrollTop = element[0].scrollTop,
+                  scrollHeight = element[0].scrollHeight,
+                  clientHeight = element[0].clientHeight,
+                  atEnd = scrollTop + clientHeight >= scrollHeight,
+                  newScrollTop = atEnd ? 0 : scrollTop + glConstants.scroll.step;
 
-            $(element).animate({
-              scrollTop: newScrollTop
-            }, glConstants.scroll.animation);
-          }, glConstants.scroll.interval);
+              $(element).animate({
+                scrollTop: newScrollTop
+              }, glConstants.scroll.animation);
+            }, glConstants.scroll.interval);            
+          }, scope.delay * 1000);
 
           scope.$on('$destroy', function() {
             $interval.cancel(scrolling);
