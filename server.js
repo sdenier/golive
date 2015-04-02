29e0cc2bd7877edba396d6de353422754513f20d
Simon Denier <simon@sogilis.com>
2015-04-02 20:17:57 +0200

    feat servers.js: put a very basic memory cache for proxy response

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var http = require('http');
 var logger = require('morgan');
 var Path = require('path');
 var request = require('request');
+var cache = require('memory-cache');
 
 exports.startServer = function startServer(port, path, callback) {
   var app = express();
@@ -17,11 +18,20 @@ exports.startServer = function startServer(port, path, callback) {
   var apiUrl = 'http://169.254.94.120:4567/json/lastresults';
   // var apiUrl = 'http://localhost:4567/json/lastresults';
   app.use('/lastresults', function(req, res) {
-    req.pipe(request(apiUrl, function(error, response, body){
-      if (error) {
-        res.status(502).send({ error: 'Connection refused: live results unavailable', code: error.code });
-      }
-    })).pipe(res);
+    var hit = cache.get('lastresults');
+    if (hit) {
+      console.log('hit cache');
+      res.json(hit);
+    } else {
+      console.log('proxy');
+      req.pipe(request(apiUrl, function(error, response, body){
+        if (error) {
+          res.status(502).send({ error: 'Connection refused: live results unavailable', code: error.code });
+        } else {
+          cache.put('lastresults', JSON.parse(response.body), 29000);
+        }
+      })).pipe(res);      
+    }
   });
 
   server.listen(port, callback);
