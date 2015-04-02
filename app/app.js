3b7c24f5089d7c7dc91675af0f7a7c0e39036b0c
Simon Denier <simon@sogilis.com>
2015-04-03 01:09:22 +0200

    feat: 2 large + 4 short columns

--- app/app.js
+++ app/app.js
@@ -15,6 +15,9 @@ angular.module('golive', [])
     }
   })
   .value('layouts', [{
+    name: '2 + 4 columns',
+    url: 'views/columns2-4.html'
+  }, {
     name: '2 + 8 columns',
     url: 'views/columns2-8.html'
   }, {
